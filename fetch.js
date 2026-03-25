05187a74daa0eefb01cdefcd1b3a8d78ba0da086
mogga <moggaoliver1@gmail.com>
2026-03-25 12:34:13 +0530

    js changes blovk

--- fetch.js
+++ fetch.js
@@ -14,3 +14,20 @@ async function getuser() {
 }
 
 getuser();
+
+async function getuser() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+
+    users.forEach(user => {
+      // console.log(user.name);
+      document.body.innerHTML += <p>${user.id}-{user.name}</p>
+    });
+
+  } catch (err) {
+    console.log('error', err);
+  }
+}
+
+getuser();
